62aa7ead9cb4ce25f9c2b38525b0cea8ce80e888
SmallJoker <mk939@ymail.com>
2021-01-02 19:31:08 +0100

    Upload skeleton

---
+++ connection.cpp
@@ -0,0 +1,16 @@
+
+Connection::Connection(const std::string &address, int port)
+{
+    // Open
+}
+
+Connection::~Connection()
+{
+    close();
+}
+
+Connection::close()
+{
+    // Disconnect
+	
+}
